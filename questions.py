f7c5b15d49d69a3f8d17d3307907f5861d9eaa8b
RT Flores <rotyflo@gmail.com>
2018-11-30 14:05:23 -0600

    Remove capitalization to read short words more easily

--- questions.py
+++ questions.py
@@ -2,17 +2,17 @@ from answers import answer_prompt
 from random_stuff import rand_choice
 from clear_screen import clear_screen
 
-def question_prompt(spanish_word, choices, answer):
+def question_prompt(word, choices, answer):
     """Prints out question prompt"""
     clear_screen()
 
-    print(f'Translate: {spanish_word.capitalize()}')
+    print(f'Translate: {word}')
     
     choices = rand_choice(choices)
     option = 1
 
     for choice in choices:
-        print(f'\n {option}) {choice.capitalize()}')
+        print(f'\n {option}) {choice}')
         option += 1
 
     choice = answer_prompt(answer, choices)
