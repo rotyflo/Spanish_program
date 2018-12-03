3e3654c7cbc173c5b1539e4638fa336b975e1f22
German Rodriguez <GermanRodriguezWork@Gmail.com>
2018-12-03 14:02:57 -0600

    Removed a test line from earlier.

--- write_to.py
+++ write_to.py
@@ -2,7 +2,6 @@ def write_to(file_path, word, difficulty):
     """Write input to file path"""
     read_file = open(file_path, 'r').read()
     write_file = open(file_path, 'w')
-    print(type(read_file))
     known_words = eval(read_file)
 
     known_words[difficulty].append(word)
