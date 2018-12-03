f142ee2d1d1425c27c048880c86bf9c1dac401cd
German Rodriguez <GermanRodriguezWork@Gmail.com>
2018-12-03 14:03:35 -0600

    Dict2set added

--- refresher.py
+++ refresher.py
@@ -9,9 +9,15 @@ def file2dict (file_path):
 	return python_dict
 
 
+def dict2set (dictionary):
+	known_words_set = set()
+	for key, value in dictionary.items():
+		for v in value:
+			known_words_set.add(v)
+	return known_words_set
+
 clear_screen()
 print('Type "exit" to end program\n')
 
 known_words = file2dict('known_words.dictionary')
-for key, value in known_words.items():
-	print(value)
+known_words_set = dict2set(known_words)
