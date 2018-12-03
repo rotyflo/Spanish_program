9f3bee29d9aa936754bc38e60fb58ac99d503cd0
German Rodriguez <GermanRodriguezWork@Gmail.com>
2018-12-03 11:33:54 -0600

    The close() method on read_file was not needed and crashed due to already being closed.

--- write_to.py
+++ write_to.py
@@ -2,14 +2,12 @@ def write_to(file_path, word, difficulty):
     """Write input to file path"""
     read_file = open(file_path, 'r').read()
     write_file = open(file_path, 'w')
-
+    print(type(read_file))
     known_words = eval(read_file)
 
     known_words[difficulty].append(word)
 
     write_file.write(str(known_words))
-    
-    read_file.close()
     write_file.close()
-    
+
     return
