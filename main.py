bc384aa83b06286e89b498ea4bba953e712a5679
German Rodriguez <GermanRodriguezWork@Gmail.com>
2018-12-05 10:43:45 -0600

    Try and except added to account for invalid input

--- main.py
+++ main.py
@@ -10,32 +10,35 @@ print("\t1) Multiple Choice")
 print("\t2) Fill-In the Blank")
 print("\t3) Change Language")
 print("\t4) Other")
-
 selection = input("\nEnter 1 - 4 to choose: ")
 
-if selection.lower() == 'exit':
-	exit()
+try:
+	if selection.lower() == 'exit':
+		exit()
 
-elif int(selection) in range(1, 5):
-	selection = int(selection)
+	elif int(selection) in range(1, 5):
+		selection = int(selection)
 
-	if selection == 1:
-		multiple_choice()
+		if selection == 1:
+			multiple_choice()
 
-	elif selection == 2:
-		# fill_in_blank()
-		clear_screen()
-		print("Fill-in the blank mode is coming soon!")
+		elif selection == 2:
+			# fill_in_blank()
+			clear_screen()
+			print("Fill-in the blank mode is coming soon!")
 
-	elif selection == 3:
-		# change_language()
-		clear_screen()
-		print("Multiple language options are coming soon!")
+		elif selection == 3:
+			# change_language()
+			clear_screen()
+			print("Multiple language options are coming soon!")
 
-	elif selection == 4:
-		# other()
-		clear_screen()
-		print("Other features are coming soon!")
+		elif selection == 4:
+			# other()
+			clear_screen()
+			print("Other features are coming soon!")
 
-else:
-	print("Sorry, try again.")
+	else:
+		print("Sorry, try again.")
+
+except:
+	print('Invalid input, exiting program.')
